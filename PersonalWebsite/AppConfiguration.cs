0f64ee48b6b44555a9395a932db1611fc3dc3ecf
Kamyab7 <valipourkamyab.official@gmail.com>
2023-12-11 21:54:53 +0330

    make personal info dynamic

--- PersonalWebsite/AppConfiguration.cs
+++ PersonalWebsite/AppConfiguration.cs
@@ -3,4 +3,16 @@
 public sealed record AppConfiguration
 {
     public string Owner { get; init; }
+
+    public string JobTitle { get; init; }
+
+    public string EmailAddress { get; init; }
+
+    public string Birthday { get; init; }
+
+    public string Location { get; set; }
+
+    public string Phone { get; init; }
+
+    public List<string> AboutMe { get; init; }
 }
--- PersonalWebsite/AppConfigurationFactory.cs
+++ PersonalWebsite/AppConfigurationFactory.cs
@@ -6,9 +6,17 @@ public static class AppConfigurationFactory
     {
         ArgumentNullException.ThrowIfNull(config);
 
+        var aboutMe = config.GetSection(nameof(AppConfiguration.AboutMe)).Get<List<string>>();
+
         var configuration = new AppConfiguration
         {
             Owner = config[nameof(AppConfiguration.Owner)],
+            JobTitle = config[nameof(AppConfiguration.JobTitle)],
+            EmailAddress = config[nameof(AppConfiguration.EmailAddress)],
+            Birthday = config[nameof(AppConfiguration.Birthday)],
+            Location = config[nameof(AppConfiguration.Location)],
+            Phone = config[nameof(AppConfiguration.Phone)],
+            AboutMe = aboutMe,
         };
 
         return configuration;
